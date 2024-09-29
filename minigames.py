bbf995924a420f6dcfcb2eb0b2f1ea372d15fae5
stopmind <smaxim120@gmail.com>
2024-09-29 19:51:32 +0300

    tictactoe

--- minigames.py
+++ minigames.py
@@ -62,7 +62,6 @@ def sapper_get_buttons(game: SapperGame) -> InlineKeyboardMarkup:
 
     return builder.as_markup()
 
-
 def gen_map() -> list[SapperCell]:
     result = []
 
@@ -101,12 +100,105 @@ def gen_map() -> list[SapperCell]:
 
     return result
 
+lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+class TicTacToeGame(BaseModel):
+    PLAYER_NONE: ClassVar[int] = 0
+    PLAYER_USER: ClassVar[int] = 1
+    PLAYER_BOT:  ClassVar[int] = 2
+
+    game_map: list[int]
+    is_finished: bool
+    chat_id: int
+    message_id: int
+    winner: int
+
+    def get_buttons(self) -> InlineKeyboardMarkup:
+        builder = InlineKeyboardBuilder()
+        builder.max_width = 3
+
+        for i in range(0, 9):
+            builder.add(InlineKeyboardButton(
+                text={
+                    TicTacToeGame.PLAYER_NONE: "▫",
+                    TicTacToeGame.PLAYER_USER: "❌",
+                    TicTacToeGame.PLAYER_BOT:  "⭕"
+                }[self.game_map[i]],
+                callback_data=f"tictactoe_cell_{i}"
+            ))
+
+        return builder.as_markup()
+
+    def check_win(self, player: int) -> bool:
+        for line in lines:
+            full = True
+            for pos in line:
+                full = full and self.game_map[pos] == player
+
+            if full:
+                self.is_finished = True
+                self.winner = player
+                return True
+
+        return False
+
+    def check_full(self) -> bool:
+        for cell in self.game_map:
+            if cell == TicTacToeGame.PLAYER_NONE:
+                return False
+
+        self.is_finished = True
+        self.winner = TicTacToeGame.PLAYER_NONE
+        return True
+
+
+def bot_check_lines(game_map: list[int], target: int) -> bool:
+    for line in lines:
+        free = -1
+        for pos in line:
+            if game_map[pos] == TicTacToeGame.PLAYER_NONE:
+                if free == -1:
+                    free = pos
+                else:
+                    free = -1
+                    break
+            elif game_map[pos] != target:
+                free = -1
+                break
+
+        if free != -1:
+            game_map[free] = TicTacToeGame.PLAYER_BOT
+            return True
+    return False
+
+def bot_do_step(game_map: list[int]):
+    if bot_check_lines(game_map, TicTacToeGame.PLAYER_BOT): return
+    if bot_check_lines(game_map, TicTacToeGame.PLAYER_USER): return
+
+    pos = randint(0, 8)
+    while game_map[pos] != TicTacToeGame.PLAYER_NONE:
+        pos = randint(0, 8)
+
+    game_map[pos] = TicTacToeGame.PLAYER_BOT
+
 class MiniGamesService(AService):
     SAPPER_WIDTH:  ClassVar[int] = 8
     SAPPER_HEIGHT: ClassVar[int] = 8
     SAPPER_BOMBS:  ClassVar[int] = 5
 
     sapper_games: dict[int, SapperGame] = Field({}, init_var=False)
+    tictactoe_games: dict[int, SapperGame] = Field({}, init_var=False)
 
     def get_sapper_game(self, chat_id: int) -> SapperGame | None:
         if chat_id in self.sapper_games:
@@ -130,6 +222,27 @@ class MiniGamesService(AService):
 
         return game
 
+    def get_tictactoe_game(self, chat_id: int) -> TicTacToeGame | None:
+        if chat_id in self.tictactoe_games:
+            result = self.tictactoe_games[chat_id]
+            if result.is_finished:
+                return None
+            return result
+        return None
+
+    def new_tictactoe_game(self, chat_id: int) -> TicTacToeGame:
+        game = TicTacToeGame(
+            game_map=[TicTacToeGame.PLAYER_NONE for _ in range(0, 9)],
+            chat_id=chat_id,
+            is_finished=False,
+            message_id=0,
+            winner=0
+        )
+
+        self.tictactoe_games[chat_id] = game
+
+        return game
+
     def init(self, bot: Bot, dispatcher: Dispatcher, controller: ServicesController):
 
         @dispatcher.message(Command("sapper"))
@@ -140,7 +253,6 @@ class MiniGamesService(AService):
                 reply_markup=sapper_get_buttons(game),
                 chat_id=message.chat.id
             )).message_id
-
 
         async def update_sapper_message(game: SapperGame):
             state = ""
@@ -157,7 +269,7 @@ class MiniGamesService(AService):
 
         @dispatcher.callback_query(lambda a: a.data.startswith("sapper_set_"))
         async def callback_sapper_set(callback: CallbackQuery):
-            game = self.get_sapper_game(callback.from_user.id)
+            game = self.get_sapper_game(callback.message.chat.id)
             if game is None: return await callback.answer()
 
             game.use_flags = callback.data.endswith("flag")
@@ -166,7 +278,7 @@ class MiniGamesService(AService):
 
         @dispatcher.callback_query(lambda a: a.data.startswith("sapper_cell_"))
         async def callback_sapper_cell(callback: CallbackQuery):
-            game = self.get_sapper_game(callback.from_user.id)
+            game = self.get_sapper_game(callback.message.chat.id)
             if game is None: return await callback.answer()
 
             cell_pos = int(callback.data.replace("sapper_cell_", ""))
@@ -230,4 +342,50 @@ class MiniGamesService(AService):
 
                 await update_sapper_message(game)
 
+            await callback.answer()
+
+        @dispatcher.message(Command("tictactoe"))
+        async def cmd_tictactoe(message: Message):
+            game = self.new_tictactoe_game(message.chat.id)
+            game.message_id = (await bot.send_message(
+                text="В процессе",
+                chat_id=message.chat.id,
+                reply_markup=game.get_buttons()
+            )).message_id
+
+        async def update_tictactoe_message(game: TicTacToeGame):
+            text = "В процессе"
+
+            if game.is_finished:
+                text = {
+                    TicTacToeGame.PLAYER_NONE: "Ничья.",
+                    TicTacToeGame.PLAYER_USER: "Победа игрока.",
+                    TicTacToeGame.PLAYER_BOT:  "Победа бота."
+                }[game.winner]
+
+            await bot.edit_message_text(
+                text,
+                reply_markup=game.get_buttons(),
+                chat_id=game.chat_id,
+                message_id=game.message_id
+            )
+
+        @dispatcher.callback_query(lambda a: a.data.startswith("tictactoe_cell_"))
+        async def callback_tictactoe_cell(callback: CallbackQuery):
+            game = self.get_tictactoe_game(callback.message.chat.id)
+            if game is None: return await callback.answer()
+
+            cell_pos = int(callback.data.replace("tictactoe_cell_", ""))
+            if game.game_map[cell_pos] != TicTacToeGame.PLAYER_NONE: return await callback.answer()
+
+            while True:
+                game.game_map[cell_pos] = TicTacToeGame.PLAYER_USER
+                if game.check_win(TicTacToeGame.PLAYER_USER): break
+                if game.check_full(): break
+
+                bot_do_step(game.game_map)
+                game.check_win(TicTacToeGame.PLAYER_BOT)
+                break
+
+            await update_tictactoe_message(game)
             await callback.answer()
